321d63c00978bab78dea9efff92da0a8f34a9fe4
Bryan <bryanmontero81@gmail.com>
2022-06-18 23:19:31 -0400

    Tarea 5 completada

---
+++ readme.md
@@ -0,0 +1,20 @@
+# Tarea 5 - Programación Web
+
+- Nombre: Bryan Xavier Florentino Montero
+- Matrícula: 2020-10674
+
+## Servicio web con NodeJs y Express
+
+Haga un servicio Web utilizando la librería express de nodeJS que haga las dos
+operaciones básicas de listar y almacenar contactos realizados por el servicio
+http://www.raydelto.org/agenda.php el cual usted ha utilizado en las dos tarea
+anteriores.
+
+## Capturas de pantalla
+
+### Peticion post al servidor con Postman 
+[!Peticion Post](img/Captura%20Post.png)
+### Peticion Get al servidor (desde un navegador) 
+[!Peticion Get](img/Captura%20Get.png)
+### Peticion Get al servidor (desde un navegador) 
+[!Peticion GetPattern](img/Captura%20GetByPattern.png)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,73 @@
 const express = require('express');
 const app = express();
-const vehicles = require('./routes/vehicles-route')
-const phone = require('./routes/phones-route')
-const computers = require('./routes/computers-route')
-
 app.use(express.json())
-app.use('/vehicle', vehicles)
-app.use('/phone', phone)
-app.use('/computers', computers)
+
+const contacts = [
+     {
+         "nombre" : "Jose",
+         "apellido": "Mella",
+         "telefono": "849-251-6812",
+     },
+     {
+        "nombre" : "Dilcio",
+         "apellido": "Hernandez",
+         "telefono": "809-236-1999",
+     },
+     {
+         "nombre" : "Dionicia",
+         "apellido": "Sosa",
+         "telefono": "849-836-7365",
+    }
+]
+
+app.get('/', (req, res) =>{
+     
+     res.send("Este servicio web contiene una lista de celulares, vehiculos y computadoras a los cuales usted puede acceder realizando una petición HTTP. También puede registrar nuevos elementos para cada lista haciendo una petición de tipo POST. Para ver los contactos agregue '/contact' a la URL")
+})
+
+app.post('/contact', (req, res) => {
+// Almacenar un nuevo contacto
+    
+    const contact = {
+        nombre : req.body.nombre,
+        apellido: req.body.apellido,
+        telefono: req.body.telefono,
+    }
+
+    contacts.push(contact)
+    
+    const response = {
+        data: contact,
+        message: "Se ha agregado con exito el nuevo contacto",
+        exito: true
+    }
+
+    res.send(response)
+})
+
+app.get('/contact', (req, res) => {
+// Obtener todos los contactos de la lista de contactos
+        
+    const response = {
+        data: contacts,
+        exito: true
+    }
+    res.send(response)
+})
+
+app.get('/contact/:searchPattern', (req, res) => {
+// Obtener todos los contactos que coincidan con un patrón de búsqueda dado (puede ser el inicio de una letra inicial o un contacto el cual su nombre empiece por dicho patrón).
+
+     matchedContacts = contacts.filter(contact => 
+                                   contact.nombre.toLowerCase().startsWith(req.params.searchPattern.toLowerCase()));
+
+     const response = {
+          data: matchedContacts,
+          exito: true
+      }
+      res.send(response)
+})
 
 app.listen(80, () => {
      console.log("El servidor está escuchando en el puerto 80")
-
-     console.log("\nEste servicio web contiene una lista de celulares, vehiculos y computadoras a los cuales usted puede acceder realizando una petición HTTP. También puede registrar nuevos elementos para cada lista haciendo una petición de tipo POST")
-
 })
